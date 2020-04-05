e29fdb7efe194986f576195962ea8a5c6858b359
nanter frechat <nanter1986@gmail.com>
2020-04-05 17:13:01 +0300

    creating csv

--- js/myjs.js
+++ js/myjs.js
@@ -6,44 +6,33 @@ var config = {
         default: 'arcade',
         arcade: {
             gravity: {
-                y: 200
+                y: 0
             }
         }
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
+var map;
+var cursors;
+var debugGraphics;
+var helpText;
+var player;
+var showDebug = false;
 
 function preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
-}
-
-function create() {
-    this.add.image(400, 300, 'sky');
-
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: {
-            start: 1,
-            end: 0
-        },
-        blendMode: 'ADD'
+    this.load.image('tiles', 'assets/magecity_1.png');
+    this.load.tilemapCSV('map', 'assets/csv_mage.csv');
+    this.load.spritesheet('player', 'assets/player.png', {
+        frameWidth: 16,
+        frameHeight: 16
     });
 
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
 }
+
+function create() {}
