5d9418c1bcce7847cc0f3ad5082f27c9e2aed59d
nanter frechat <nanter1986@gmail.com>
2020-04-09 17:29:13 +0300

    just a sprite

--- js/myjs.js
+++ js/myjs.js
@@ -1,115 +1,87 @@
-console.log("start");
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y: 0
-            }
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+function Room(game) {
+    console.log("start");
+	this.sprite=null;
+
+    function preload() {
+        console.log("preload1");
+        //add python server into file
+        //game.load.tilemap('map', '../assets/csv_mage.csv', null, Phaser.Tilemap.CSV);
+        //this.load.image('tiles', 'assets/magecity_1.png');
+        game.load.spritesheet('player', 'assets/zombie-male-base.png', 16, 16);
+        //find sprite and load it
+        //load a map of tiles
+        //move a character in it
+        //have collission with walls
+        //have door to other scene
+        console.log("preload2");
+
     }
-};
 
-console.log("config:");
-console.log(config);
+    function create() {
+        console.log("create1");
+        //**doc**
+        //map = game.add.tilemap('map', 16, 16);
+        //map.addTilesetImage('tiles')
+        //layer = map.createLayer(0);
+        //layer.resizeWorld()
+        //map.setCollisionBetween(54, 83);
+        //layer.debug = true;
 
-var game = new Phaser.Game(config);
-console.log("game:" + game);
-var map;
-var cursors;
-var debugGraphics;
-var helpText;
-var player;
-var showDebug = false;
-var layer;
+        //  Player
+        this.sprite= game.add.sprite(48, 48, 'player', 1);
+        //player.animations.add('left', [8, 9], 10, true);
+        //player.animations.add('right', [1, 2], 10, true);
+        //player.animations.add('up', [11, 12, 13], 10, true);
+        //player.animations.add('down', [4, 5, 6], 10, true);
 
-function preload() {
-    console.log("preload1");
-    //add python server into file
-    //game.load.tilemap('map', '../assets/csv_mage.csv', null, Phaser.Tilemap.CSV);
-    //this.load.image('tiles', 'assets/magecity_1.png');
-    //game.load.spritesheet('player', 'assets/zombie-male-base.png', 16, 16);
-    //find sprite and load it
-    //load a map of tiles
-    //move a character in it
-    //have collission with walls
-    //have door to other scene
-    console.log("preload2");
+        //game.physics.enable(player, Phaser.Physics.ARCADE);
 
-}
+        player.body.setSize(10, 14, 2, 1);
 
-function create() {
-    console.log("create1");
-    //**doc**
-    //map = game.add.tilemap('map', 16, 16);
-    //map.addTilesetImage('tiles')
-    //layer = map.createLayer(0);
-    //layer.resizeWorld()
-    //map.setCollisionBetween(54, 83);
-    //layer.debug = true;
+        game.camera.follow(player);
 
-    //  Player
-    this.player = this.game.add.sprite(48, 48, 'player', 1);
-    //player.animations.add('left', [8, 9], 10, true);
-    //player.animations.add('right', [1, 2], 10, true);
-    //player.animations.add('up', [11, 12, 13], 10, true);
-    //player.animations.add('down', [4, 5, 6], 10, true);
+        cursors = game.input.keyboard.createCursorKeys();
 
-    //game.physics.enable(player, Phaser.Physics.ARCADE);
+        var help = game.add.text(16, 16, 'Arrows to move', {
+            font: '14px Arial',
+            fill: '#ffffff'
+        });
 
-    player.body.setSize(10, 14, 2, 1);
-
-    game.camera.follow(player);
-
-    cursors = game.input.keyboard.createCursorKeys();
-
-    var help = game.add.text(16, 16, 'Arrows to move', {
-        font: '14px Arial',
-        fill: '#ffffff'
-    });
-
-    help.fixedToCamera = true;
-    console.log("create2");
+        help.fixedToCamera = true;
+        console.log("create2");
 
 
 
-}
+    }
 
-function update() {
-    console.log("replace1");
-    //game.physics.arcade.collide(player, layer);
-    player.body.velocity.set(0)
-    //if (cursors.left.isDown) {
-    //    player.body.velocity.x = -100;
-    //    player.play('left');
-    //} else if (cursors.right.isDown) {
-    //    player.body.velocity.x = 100;
-    //    player.play('right');
-    //} else if (cursors.up.isDown) {
-    //    player.body.velocity.y = -100;
-    //    player.play('up');
-    //} else if (cursors.down.isDown) {
-    //    player.body.velocity.y = 100;
-    //    player.play('down');
-    //} else {
-    //    player.animations.stop();
-    //}
-    console.log("replace2");
+    function update() {
+        console.log("replace1");
+        //game.physics.arcade.collide(player, layer);
+        player.body.velocity.set(0)
+        //if (cursors.left.isDown) {
+        //    player.body.velocity.x = -100;
+        //    player.play('left');
+        //} else if (cursors.right.isDown) {
+        //    player.body.velocity.x = 100;
+        //    player.play('right');
+        //} else if (cursors.up.isDown) {
+        //    player.body.velocity.y = -100;
+        //    player.play('up');
+        //} else if (cursors.down.isDown) {
+        //    player.body.velocity.y = 100;
+        //    player.play('down');
+        //} else {
+        //    player.animations.stop();
+        //}
+        console.log("replace2");
 
-}
+    }
 
-function render() {
-    console.log("render1");
+    function render() {
+        console.log("render1");
 
-    game.debug.body(player);
+        game.debug.body(player);
 
-    console.log("render2");
+        console.log("render2");
+    }
 }
